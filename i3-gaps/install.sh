15e3bbfa0db07970c0ceb4c78b32458f02655f18
Florian Boulestreau <florian.boulestreau@grandluxurygroup.com>
2018-05-30 14:50:29 +0200

    Fix lock shortcut Move polybar scrit to screen group Delete duplicated Simplify lock script

--- i3-gaps/install.sh
+++ i3-gaps/install.sh
@@ -5,4 +5,4 @@ DEST_DIR=$HOME/.config/i3
 createFolder $DEST_DIR
 
 createLink i3-gaps/scripts $DEST_DIR/scripts
-createLink i3-gaps/config._melchizedek $DEST_DIR/config
+createLink i3-gaps/config $DEST_DIR/config
